896d6148cb3e6731cd30c7809cf999f39979a8c6
RainierJared <116169371+RainierJared@users.noreply.github.com>
2025-03-02 23:04:49 +1300

    Added same feature to table page

---
+++ airtable-clone/src/app/Table/layout.tsx
@@ -0,0 +1,21 @@
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+import { TRPCReactProvider } from "~/trpc/react";
+
+export const metadata: Metadata = {
+    title: "Untitled Base",
+    description: "Airtable Clone by Rainier Jared",
+    icons: [{ rel: "icon", url: "/favicon.ico" }],
+  };
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body>
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+      </body>
+    </html>
+  );
+}
