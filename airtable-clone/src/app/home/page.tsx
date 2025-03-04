91633b64b877075e7d558c849dd3cf731823240b
RainierJared <116169371+RainierJared@users.noreply.github.com>
2025-03-05 02:21:40 +1300

    Fixing vercel error

--- airtable-clone/src/app/home/page.tsx
+++ airtable-clone/src/app/home/page.tsx
@@ -1,25 +1,25 @@
 import NavBar from "../_components/navbar";
 
-export const HomePage = () => {
+import React from 'react'
+
+export default function HomePage() {
   return (
     <div className="flex flex-col items-center">
-      <div className="flex-none  w-full">
-        <header className="w-full border-b-2">
-          <NavBar />
-        </header>
-      </div>
+    <div className="flex-none  w-full">
+      <header className="w-full border-b-2">
+        <NavBar />
+      </header>
+    </div>
 
-      <a href="/table">
-      <div className="rounded-[5] bg-blue-900 text-white mt-2 p-1">
-        <h1 className="font-bold">Go to Untitled Base</h1>
-      </div>
-      </a>
+    <a href="/table">
+    <div className="rounded-[5] bg-blue-900 text-white mt-2 p-1">
+      <h1 className="font-bold">Go to Untitled Base</h1>
+    </div>
+    </a>
 
-      <div>
-        <h1 className="font-bold">Welcome to Airtable 2 Test</h1>
-      </div>
-  </div>
-  );
+    <div>
+      <h1 className="font-bold">Welcome to Airtable 2 Test</h1>
+    </div>
+</div>
+  )
 }
-
-export default HomePage;
--- airtable-clone/src/app/page.tsx
+++ airtable-clone/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { HomePage } from "./home/page";
+import HomePage from "./home/page";
 
 export default async function App() {
   return (
