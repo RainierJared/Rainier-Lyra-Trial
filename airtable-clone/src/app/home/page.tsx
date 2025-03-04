4c96f11e52e66c21ccba8439696f27ae1daae66c
RainierJared <116169371+RainierJared@users.noreply.github.com>
2025-03-04 22:16:02 +1300

    Preparing the file directory for dynamic pages
    Moved the home page to its own folder, and made the main app folder's page.tsx call the Home page function.

---
+++ airtable-clone/src/app/home/page.tsx
@@ -0,0 +1,25 @@
+import NavBar from "../_components/navbar";
+
+export const HomePage = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex-none  w-full">
+        <header className="w-full border-b-2">
+          <NavBar />
+        </header>
+      </div>
+
+      <a href="/table">
+      <div className="rounded-[5] bg-blue-900 text-white mt-2 p-1">
+        <h1 className="font-bold">Go to Untitled Base</h1>
+      </div>
+      </a>
+
+      <div>
+        <h1 className="font-bold">Welcome to Airtable 2</h1>
+      </div>
+  </div>
+  );
+}
+
+export default HomePage;
--- airtable-clone/src/app/page.tsx
+++ airtable-clone/src/app/page.tsx
@@ -1,23 +1,9 @@
-import NavBar from "./_components/navbar";
+import { HomePage } from "./home/page";
 
-export default async function Home() {
+export default async function App() {
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex-none  w-full">
-        <header className="w-full border-b-2">
-          <NavBar />
-        </header>
-      </div>
-
-      <a href="/table">
-      <div className="rounded-[5] bg-blue-900 text-white mt-2 p-1">
-        <h1 className="font-bold">Go to Untitled Base</h1>
-      </div>
-      </a>
-
-      <div>
-        <h1 className="font-bold">Welcome to Airtable 2</h1>
-      </div>
-  </div>
+    <div>
+      <HomePage />
+    </div>
   );
 }
